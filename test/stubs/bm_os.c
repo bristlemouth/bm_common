db15c7963f18c1e74788806b4b538a0955ad33ca
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-18 11:46:14 -0700

    define it as a void fake

--- test/stubs/bm_os.c
+++ test/stubs/bm_os.c
@@ -6,7 +6,7 @@ void bm_free(void *p) { return free(p); }
 DEFINE_FAKE_VALUE_FUNC(BmSemaphore, bm_semaphore_create);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_semaphore_give, BmSemaphore);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_semaphore_take, BmSemaphore, uint32_t);
-DEFINE_FAKE_VALUE_FUNC(void, bm_delay, uint32_t);
+DEFINE_FAKE_VOID_FUNC(bm_delay, uint32_t);
 DEFINE_FAKE_VALUE_FUNC(uint32_t, bm_get_tick_count);
 DEFINE_FAKE_VALUE_FUNC(uint32_t, bm_ms_to_ticks, uint32_t);
 DEFINE_FAKE_VALUE_FUNC(uint32_t, bm_ticks_to_ms, uint32_t);
