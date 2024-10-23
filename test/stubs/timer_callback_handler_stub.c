83bcda25620175cbe513adce9be78968e77cc0eb
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-23 12:49:42 -0700

    don't actually need the init

--- test/stubs/timer_callback_handler_stub.c
+++ test/stubs/timer_callback_handler_stub.c
@@ -1,4 +1,3 @@
 #include "mock_timer_callback_handler.h"
-
-DEFINE_FAKE_VALUE_FUNC(BmErr, timer_callback_handler_init);
+#
 DEFINE_FAKE_VALUE_FUNC(bool, timer_callback_handler_send_cb, timer_handler_cb, void*, uint32_t);
