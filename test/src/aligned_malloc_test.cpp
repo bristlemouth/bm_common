fe5e492b486294d7b3e1dcd00526aca03b623a97
Zachary Crockett <zachary.crockett@sofarocean.com>
2024-10-24 15:28:38 -0700

    fix: even though it's a test, be a good citizen and clean up

--- test/src/aligned_malloc_test.cpp
+++ test/src/aligned_malloc_test.cpp
@@ -23,56 +23,68 @@ TEST(AlignedMalloc, happy_path) {
 TEST(AlignedMalloc, alignment_must_be_power_of_two) {
   void *ptr = aligned_malloc(3, 16);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
   ptr = aligned_malloc(5, 16);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
   ptr = aligned_malloc(6, 16);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
   ptr = aligned_malloc(7, 16);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, size_zero_not_allowed) {
   void *ptr = aligned_malloc(16, 0);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, alignment_zero_not_allowed) {
   void *ptr = aligned_malloc(0, 16);
   EXPECT_EQ(ptr, nullptr);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align8) {
   void *ptr = aligned_malloc(8, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 8, 0);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align16) {
   void *ptr = aligned_malloc(16, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 16, 0);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align32) {
   void *ptr = aligned_malloc(32, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 32, 0);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align64) {
   void *ptr = aligned_malloc(64, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 64, 0);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align128) {
   void *ptr = aligned_malloc(128, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 128, 0);
+  aligned_free(ptr);
 }
 
 TEST(AlignedMalloc, align256) {
   void *ptr = aligned_malloc(256, 37);
   EXPECT_NE(ptr, nullptr);
   EXPECT_EQ((uintptr_t)ptr % 256, 0);
+  aligned_free(ptr);
 }
